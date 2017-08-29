0cdf338fcc06c81624578d82a44e3440b67bb0f1
Madhur Chauhan <madhur4127@gmail.com>
2017-08-29 14:03:06 +0000

    Removed redundant whitespaces

--- mirror_pgm.cpp
+++ mirror_pgm.cpp
@@ -1,17 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 int main(){
-  freopen("ip.pgm", "r", stdin);
-  freopen("op.pgm", "w", stdout);
-  cout<<"P2\n";
-  string s; getline(cin,s); getline(cin,s);
+  freopen("ip.pgm", "r", stdin);  freopen("op.pgm", "w", stdout);
+  string s; getline(cin,s); cout<<s<<endl; getline(cin,s); cout<<s<<endl;
   int r,c,b ;cin>>r>>c>>b; cout<<r<<" "<<c<<endl<<b<<endl; int vec[c];
   for(int i=0;i<r;++i){
     for(int j=0;j<c;++j) cin>> vec[j];
-    for(int j=c-1;j>=0;--j) cout<<vec[j]<<" ";
-    cout<<endl;
+    for(int j=c-1;j>=0;--j) cout<<vec[j]<<" ";    cout<<endl;
   }
   return 0;
 }
