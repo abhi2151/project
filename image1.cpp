78a5ef69cc47ff1b6c7f8cfd129f933268b2bb8f
Utkarsh-1 <31444236+Utkarsh-1@users.noreply.github.com>
2017-09-04 21:35:40 +0530

    Add files via upload

---
+++ image1.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  freopen("op.pgm", "w", stdout);
+  //image type P2 and file name.
+  cout<<"P3"<<endl;
+  //r=number of rows, c=number of columns, b=maximum intensity value in image, vec[c]=array of elements of same row.
+  int r=10,c=10,b=255 ;
+  cout<<r<<" "<<c<<endl<<b<<endl; int vec_r[r][c], vec_g[r][c], vec_b[r][c];
+  for(int i=0;i<r;i++)
+  {
+    for(int j=0;j<c;j++) 
+	{
+		if(i==j) 
+		{
+			vec_r[i][j]=255;
+			vec_b[i][j]=0;
+			vec_g[i][j]=0;
+		} 
+		else
+		{
+			vec_r[i][j]=0;
+			if(i>j)
+			{
+				vec_b[i][j]=255;
+				vec_g[i][j]=0;
+			}
+			else
+			{
+				vec_g[i][j]=255;
+				vec_b[i][j]=0;
+			}
+		} 
+	}
+  }
+  for(int i=0;i<r;i++)
+  {
+    for(int j=0;j<c;j++) 
+	{
+		printf("%3d %3d %3d ", vec_r[i][j], vec_g[i][j], vec_b[i][j]);
+	}
+	/*cout<<"\t";
+	for(int j=0;j<c;j++) 
+	{
+		printf("%3d ", vec_g[i][j]);
+	}
+	cout<<"\t";
+	for(int j=0;j<c;j++) 
+	{
+		printf("%3d ", vec_b[i][j]);
+	}*/
+	cout<<endl;
+  }  
+  return 0;
+}
+
